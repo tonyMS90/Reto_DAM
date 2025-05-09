21f273e6e2a146a1aff60e355925f55560197acd
martiniglesias21 <rocococo2003@gmail.com>
2025-05-09 19:46:43 +0200

    Subida proyecto app para usuario

---
+++ RetoDam/app/src/main/java/com/example/retodam/SolicitudesSimuladas.kt
@@ -0,0 +1,5 @@
+package com.example.retodam
+
+object SolicitudesSimuladas {
+    val listaSolicitudes = mutableListOf<Solicitud>()
+}
---
+++ RetoDam/app/src/main/java/com/example/retodam/UsuarioSession.kt
@@ -0,0 +1,5 @@
+package com.example.retodam
+
+object UsuarioSession {
+    var usuarioActual: Usuario?= null
+}
